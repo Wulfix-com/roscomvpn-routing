5ebeaf47a2c19249cdeec75c8b6f15d05f7b4bd7
Wulfix-com <thetoppets@yandex.ru>
2026-08-30 01:21:47 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,15 +1,18 @@
-import urllib.request
 import json
 
-url = "https://raw.githubusercontent.com/hydraponique/roscomvpn-routing/main/HAPP/DEFAULT.json"
+file_path = "HAPP/DEFAULT.json"
 
-response = urllib.request.urlopen(url)
-text = response.read()
-data = json.loads(text)
+with open(file_path, "r", encoding="utf-8") as file:
+    data = json.load(file)
 
-data["ProxySites"] = ["geosite:meta"] + data["ProxySites"]
-data["DirectSites"] = ["domain:dtpax.ru", "domain:tvoe.live"] + data["DirectSites"]
+my_proxy = ["geosite:meta"]
+my_direct = ["domain:dtpax.ru", "domain:tvoe.live"]
 
-file = open("HAPP/DEFAULT.json", "w", encoding="utf-8")
-json.dump(data, file, indent=4, ensure_ascii=False)
-file.close()
+data["ProxySites"] = [site for site in data["ProxySites"] if site not in my_proxy]
+data["DirectSites"] = [site for site in data["DirectSites"] if site not in my_direct]
+
+data["ProxySites"] = my_proxy + data["ProxySites"]
+data["DirectSites"] = my_direct + data["DirectSites"]
+
+with open(file_path, "w", encoding="utf-8") as file:
+    json.dump(data, file, indent=4, ensure_ascii=False)
